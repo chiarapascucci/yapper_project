b234cc6a9829c5193f86aaf15f75be1c438df43e
Chiara Pascucci <ch.pascucci@gmail.com>
2021-08-03 17:42:12 +0100

    fixed run time error. committing working version

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,3 +1,4 @@
+
 from django.urls import path
 from rango import views
 
@@ -5,13 +6,13 @@ app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index/', views.index, name = 'index'),
     path('about/', views.about, name='about'),
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
     path('add_category/', views.add_category, name='add_category'),
     path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
     path('restricted/', views.restricted, name='restricted'),
-    
-
+ 
     path('sports/', views.sports_homepage, name='sports'),                          # =========== Start of yapper urls
     path('sports/<slug:sports_name_slug>/', views.sports_profile, name='sports_name'),        
     path('breeds/', views.breed_homepage ,name='breeds'),
