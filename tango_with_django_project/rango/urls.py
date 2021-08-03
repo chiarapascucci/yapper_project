eb3a402da0c9ca2f59b0864a4aa066eaf422548a
Chiara Pascucci <ch.pascucci@gmail.com>
2021-08-03 13:47:05 +0100

    implemented redux user auth

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -9,11 +9,8 @@ urlpatterns = [
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
     path('add_category/', views.add_category, name='add_category'),
     path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
-    path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
     path('restricted/', views.restricted, name='restricted'),
-    path('logout/', views.user_logout, name='logout'),
-
+    
 
     path('sports/', views.sports_homepage, name='sports'),                          # =========== Start of yapper urls
     path('sports/sports_name/', views.sports_profile, name='sports_name'),          # Requires dynamic slug field
