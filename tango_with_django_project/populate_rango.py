e40cbc4730abe8aedcf89d4075a930a456be383d
Nicholas Cheong <nicholas_cheong141@outlook.com>
2021-08-05 08:35:54 +0200

    Cleaned populations cript

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -8,42 +8,8 @@ from rango.models import Category, Page, Sport, Dog, Competition, Breed, Award, 
 # For an explanation of what is going on here, please refer to the TwD book.
 
 def populate():
-    python_pages = [
-        {'title': 'Official Python Tutorial',
-         'url':'http://docs.python.org/3/tutorial/',
-         'views': 114,},
-        {'title':'How to Think like a Computer Scientist',
-         'url':'http://www.greenteapress.com/thinkpython/',
-         'views': 53},
-        {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net/tutorials/python/',
-         'views': 20} ]
     
-    django_pages = [
-        {'title':'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
-         'views': 32},
-        {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/',
-         'views': 12},
-        {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/',
-         'views': 1258} ]
-    
-    other_pages = [
-        {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/',
-         'views': 54},
-        {'title':'Flask',
-         'url':'http://flask.pocoo.org',
-         'views': 64} ]
-    
-    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
-            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
-            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
-
     # Yapper population
-
     jumping_competitions = [ 
         {'name':'Comp A',
          'description':'Comp A description',
@@ -124,12 +90,6 @@ def populate():
                     'follows': 1}}
     
 
-    for cat, cat_data in cats.items():
-        c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
-        for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'], views=p['views'])
-
-
     # Yapper popultation
     for sport_name, sport_data in sports.items():
         sport = add_sport(sport_name,sport_data['description'], sport_data['breed_restrictions'], sport_data['follows'])
@@ -170,21 +130,6 @@ def populate():
 
 
 # Add methods 
-def add_page(cat, title, url, views=0):
-    p = Page.objects.get_or_create(category=cat, title=title)[0]
-    p.url=url
-    p.views=views
-    p.save()
-    return p
-
-def add_cat(name, views=0, likes=0):
-    c = Category.objects.get_or_create(name=name)[0]
-    c.views = views
-    c.likes = likes
-    c.save()
-    return c
-
-
 
 def add_breed(name, descrip, follows=0):
     b = Breed.objects.get_or_create(name=name)[0]
