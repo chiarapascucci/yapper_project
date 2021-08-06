804f3c085301a53d4556d46a02aa8b5642d2f29c
Nicholas Cheong <nicholas_cheong141@outlook.com>
2021-08-06 19:40:35 +0200

    Added test 3 case

--- tango_with_django_project/rango/tests.py
+++ tango_with_django_project/rango/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from django.test import Client
 from rango.models import Sport, Dog, Competition, Breed, Award, Participation
+from django.urls import reverse, resolve
 import random as rand
 import datetime
 import os
@@ -15,27 +17,36 @@ class ModelUnitTests(TestCase):
         # Instantiate differnt entites and populate  
         # Sport entity 
         sport = Sport.objects.create(name="football")
+        sport.slug = sport.name
+        sport.save()
 
         # Competition entity
         competition = Competition.objects.create(name="competition",sport=sport)
         competition.address = "address"
         competition.date = datetime.date(2021,10,22)
         competition.eventpage = "http:/www.youtube.com"
+        competition.slug = competition.name
+        competition.save()
 
         # Breed emtity
         breed = Breed.objects.create(name="malamute")
         breed.description = "description"
         breed.follows = 20
+        breed.slug = breed.name
+        breed.save()
 
         # Dog entity
         dog = Dog.objects.create(name="doggo", breed=breed)
         dog.main_about = "description"
         dog.follows = 30
+        dog.slug = dog.name
+        dog.save()
 
         # Award entity
         award = Award.objects.create(name="thebest")
         award.description = "description"
-        certificate_path = "pathtofglory"
+        award.certificate_path = "pathtofglory"
+        award.save()
 
         # Participation entity
         participation = Participation.objects.create(name = "participation", dog=dog, competition=competition, award=award)
@@ -87,6 +98,38 @@ class ModelUnitTests(TestCase):
         print("Testing Participation and Award...")
         self.assertEqual(award, participation.award, "Award and Participation relation dont match.")
 
+
+    def test_ViewResponse(self): 
+
+        print("T3: Testing view function responses - testing on 0,1,2 text slug input views - ")
+        
+        # Retrieve objects again (fine from first test)
+        sport = Sport.objects.get(name="football")
+        competition = Competition.objects.get(name="competition")
+        breed = Breed.objects.get(name="malamute")
+        dog = Dog.objects.get(name="doggo")
+        award = Award.objects.get(name="thebest")
+        participation = Participation.objects.get(name="participation")
+
+        print("Testing sport homepage view (no slug input requirement):")
+        # get response and context (context for this should be a queryset of sport)
+        response = self.client.get(reverse('rango:sports')) # kwargs={'breed_name_slug': breed.slug, 'dog_slug': dog.slug}))
+        context = response.context
+        self.assertEqual(context['sports'].first(), sport, "Context of Sport from sport homepage response does not match")
+
+        print("Testing sport profile view (one slug input requirement): ")
+        response = self.client.get(reverse('rango:sports_name', kwargs={'sports_name_slug': sport.slug})) # kwargs={'breed_name_slug': breed.slug, 'dog_slug': dog.slug}))
+        context = response.context
+        self.assertEqual(context['sport'], sport, "Context of Sport from sport profile does not match.")
+        self.assertEqual(context['competitions'].first(), competition,"Context of Competition from sport profile does not match." )
+    
+        print("Testing dog profile view (two slug input requirement): ")
+        response = self.client.get(reverse('rango:dog_profile', kwargs={'breed_name_slug': breed.slug, 'dog_slug': dog.slug})) # kwargs={'breed_name_slug': breed.slug, 'dog_slug': dog.slug}))
+        context = response.context
+        self.assertEqual(context['dog'], dog, "Context of Dog from dog profile does not match.")
+        self.assertEqual(context['breed'], breed,"Context of Breed from dog profile does not match." )
+        self.assertEqual(context['participations'].first(), participation,"Context of Participation from dog profile does not match." )
+       
 
 
 
