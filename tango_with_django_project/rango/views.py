d5ffbd742b91b7d723e4ad1bfbb5f50aa8566454
Vic J <johnson.vne@gmail.com>
2021-08-06 09:50:51 +0100

    Adjustments

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -417,6 +417,7 @@ def register_profile(request):
     if request.method == 'POST':
         form = UserProfileForm(request.POST, request.FILES)
         if form.is_valid():
+            
             user_profile = form.save(commit=False)
             user_profile.user = request.user
             user_profile.save()
